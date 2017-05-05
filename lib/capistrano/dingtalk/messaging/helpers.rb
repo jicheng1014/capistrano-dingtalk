f9a988ab802f39f341abfe52a27c93616609d9b4
atpking <atpking@gmail.com>
2017-05-05 14:09:33 +0800

    fix

--- lib/capistrano/dingtalk/messaging/helpers.rb
+++ lib/capistrano/dingtalk/messaging/helpers.rb
@@ -1,36 +1,34 @@
-module Slackistrano
-  module Messaging
-    module Helpers
+module Capistrano::Dingtalk::Messaging
+  module Helpers
 
-      def username
-        'cap-dingtalk-bot'
-      end
-
-      def deployer
-        ENV['USER'] || ENV['USERNAME']
-      end
-
-      def branch
-        fetch(:branch)
-      end
-
-      def application
-        fetch(:application)
-      end
-
-      def stage(default = 'an unknown stage')
-        fetch(:stage, default)
-      end
-
-      #
-      # Return the elapsed running time as a string.
-      #
-      # Examples: 21-18:26:30, 15:28:37, 01:14
-      #
-      def elapsed_time
-        `ps -p #{$$} -o etime=`.strip
-      end
-
+    def username
+      'cap-dingtalk-bot'
     end
+
+    def deployer
+      ENV['USER'] || ENV['USERNAME']
+    end
+
+    def branch
+      fetch(:branch)
+    end
+
+    def application
+      fetch(:application)
+    end
+
+    def stage(default = 'an unknown stage')
+      fetch(:stage, default)
+    end
+
+    #
+    # Return the elapsed running time as a string.
+    #
+    # Examples: 21-18:26:30, 15:28:37, 01:14
+    #
+    def elapsed_time
+      `ps -p #{$$} -o etime=`.strip
+    end
+
   end
 end
